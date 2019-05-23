93aeb324c5f65d45fe0559c3ecbfbe4ef14e5da8
judy <judy@Users-MacBook-Pro-4.local>
2019-05-22 20:29:53 -0700

    fixed routing

--- src/routes/groceries.js
+++ src/routes/groceries.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const groceryController = require("../controllers/groceryController")
 
-router.get("/groceries", groceryController.index);
+router.get("/", groceryController.index);
 router.get("/groceries/new", groceryController.new);
 router.post("/groceries/create", groceryController.create);
 router.get("/groceries/:id", groceryController.show);
