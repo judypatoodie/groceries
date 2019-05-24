ab655bb2cf0f2d2a9acfcd46cae1b2eb70c5c0ef
Judy Huynh <jhuynh@mossbeachhomes.com>
2019-05-24 16:19:06 -0700

    Added authorization and users

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,7 +2,9 @@ module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
     const groceryRoutes = require("../routes/groceries");
+    const userRoutes = require("../routes/users");
     app.use(staticRoutes);
     app.use(groceryRoutes);
+    app.use(userRoutes);
   }
 }
