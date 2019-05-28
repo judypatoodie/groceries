a037d1d847accdcd9db92ba6a69470b890c50cd0
Judy Huynh <jhuynh@mossbeachhomes.com>
2019-05-28 11:17:58 -0700

    cleaned up some files

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,9 +1,8 @@
 module.exports = {
   init(app){
-    const staticRoutes = require("../routes/static");
     const groceryRoutes = require("../routes/groceries");
     const userRoutes = require("../routes/users");
-    app.use(staticRoutes);
+
     app.use(groceryRoutes);
     app.use(userRoutes);
   }
--- src/routes/static.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const staticController = require("../controllers/staticController");
-const groceryController = require("../controllers/groceryController");
-router.get("/", groceryController.index);
-
-module.exports = router;
